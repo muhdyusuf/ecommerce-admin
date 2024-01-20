8ad261640cb3c83a0f039a3ce61b20d52677816d
Muhammad Yusuf <92169759+muhdyusuf@users.noreply.github.com>
2024-01-20 16:21:20 +0800

    build:add checkout api route to admin app

--- src/app/api/checkout/route.ts
+++ src/app/api/checkout/route.ts
@@ -7,7 +7,7 @@ import prisma from "../../../../prisma/client"
 
 const corsHeader={
     "Access-Control-Allow-Origin":"*",
-    "Access-Control-Allow-Methods":"POST,OPTIONS",
+    "Access-Control-Allow-Methods":"GET, POST, PUT, DELETE, OPTIONS",
     "Access-Control-Allow-Headers":"Content-Type,Authorization",
   }
   
